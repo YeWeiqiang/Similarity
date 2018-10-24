6d68de8b92e2d44845f3acac5537d179b8dc2b6e
YeWeiqiang <yeweiqiang002@gmail.com>
2018-10-24 18:33:47 +0800

    add
    。。。。。

--- src/sample/Similarity.java
+++ src/sample/Similarity.java
@@ -16,12 +16,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Similarity extends Application{
-    private static int n;
-    private static double[] x;
-    private static double[] y;
+    public static ReadXls r;
     private static double[] e;
     private static double[] c;
     private static double[] p;
@@ -30,25 +29,11 @@ public class Similarity extends Application{
     AreaChart<Number, Number> areaChart = new AreaChart<>(xAxis, yAxis);
     private TableView<dataString> tableView = new TableView<>();
     private final ObservableList<dataString> data = FXCollections.observableArrayList();
-
-    public static ReadXls getR() {
-        return r;
-    }
-
-    public static ReadXls r;
+    private int key = 0;
 
     public static void main(String[] args) throws Exception {
         r = new ReadXls();
-//        r.Printf();
-//        r.Printf();
-        n = r.rows;
-        x = new double[r.rows];
-        y = new double[r.rows];
-        for(int i = 0; i < r.rows; i++){
-            x[i] = r.output[0][i];
-            y[i] = r.output[1][i];
-//            System.out.println(x[i] + "   " + y[i]);
-        }
+        r.Printf();
 
         e = new double[r.columns];
         c = new double[r.columns];
@@ -77,17 +62,17 @@ public class Similarity extends Application{
     @Override
     public void start(Stage stage) {
         stage.setTitle("Similarity");
-
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
 
         HBox upperHBox = new HBox();
         ComboBox<String> cbo1 = new ComboBox<>();
+        ComboBox<String> cbo2 = new ComboBox<>();
+
         for (int i = 1; i <= 20; i++){
             cbo1.getItems().add("GL-" + i);
         }
         cbo1.setValue("Choose GL");
-        ComboBox<String> cbo2 = new ComboBox<>();
         for (int i = 1; i <= 20; i++){
             cbo2.getItems().add("GL-" + i);
         }
@@ -95,6 +80,7 @@ public class Similarity extends Application{
         upperHBox.getChildren().addAll(cbo1, cbo2);
 
         TableColumn data1Col = new TableColumn("Data1");
+//        data1Col.setId("lol");
         data1Col.setMinWidth(130);
         data1Col.setCellValueFactory(new PropertyValueFactory<>("data1"));
         TableColumn data2Col = new TableColumn("Data2");
@@ -104,46 +90,62 @@ public class Similarity extends Application{
         tableView.setItems(data);
         tableView.getColumns().addAll(data1Col, data2Col);
 
+        VBox leftVBox = new VBox();
+        leftVBox.setPadding(new Insets(10, 0, 0, 10));
+        leftVBox.getChildren().addAll(upperHBox, tableView);
+        gridPane.add(leftVBox, 0, 0);
+
         areaChart.setTitle("AreaChart");
         areaChart.setPrefSize(400, 600);
 
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("GL-1");
-        for(int i = 0; i < this.n; i++){
-            series1.getData().add(new XYChart.Data<>(i+1, this.r.output[0][i]));
-        }
-        areaChart.getData().add(series1);
+        cbo1.setOnAction(e->{
+            if(key == 1){
+                areaChart.getData().remove(0);
+            }
+
+            XYChart.Series series1 = new XYChart.Series();
+            int k = cbo1.getValue().length() == 4 ? Integer.parseInt(cbo1.getValue().charAt(3) + "" ): Integer.parseInt(cbo1.getValue().charAt(3) + "" + cbo2.getValue().charAt(4));
+            series1.setName("GL-" + k);
+
+            for(int i = 0; i < r.rows; i++){
+                series1.getData().add(new XYChart.Data(i+1, this.r.output[k-1][i]));
+            }
+
+            areaChart.getData().add(series1);
+        });
 
         cbo2.setOnAction(e ->{
             if(areaChart.getData().size() == 2){
                 areaChart.getData().remove(1);
             }
+
             XYChart.Series series2 = new XYChart.Series();
-            int k = Integer.parseInt(cbo2.getValue().charAt(3) + "");
+            int k = cbo2.getValue().length() == 4 ? Integer.parseInt(cbo2.getValue().charAt(3) + "" ): Integer.parseInt(cbo2.getValue().charAt(3) + "" + cbo2.getValue().charAt(4));
             series2.setName("GL-" + k);
 
-            for(int i = 0; i < this.n; i++){
+            for(int i = 0; i < r.rows; i++){
                 series2.getData().add(new XYChart.Data(i+1, this.r.output[k-1][i]));
             }
+
             areaChart.getData().add(series2);
 
-            System.out.println(data.size());
             if(data.size() != 0){
 //                data.removeAll();     //？
                 data.remove(0,35);
             }
-            for(int i = 0; i < n; i++){
+            for(int i = 0; i < r.rows; i++){
                 data.add(new dataString(""+this.r.output[0][i], ""+this.r.output[k-1][i]));
             }
 
         });
 
         HBox buttomRightHBox = new HBox();
-        Button imageButton1 = new Button("", new ImageView("euclidean.jpg"));
+        Button imageButton1 = new Button("", new ImageView("img/euclidean.jpg"));
         imageButton1.setPrefSize(200, 200);
 //        imageButton1.setOnMouseEntered(event -> {});
         imageButton1.setOnMouseClicked(e->{
-            areaChart.getData().remove(0, 1);
+            if(areaChart.getData().size() == 1) areaChart.getData().remove(0, 1);
+            else areaChart.getData().remove(0, 2);
 //            areaChart.getData().removeAll();
             XYChart.Series seriesE = new XYChart.Series();
             seriesE.setName("EuclideanMetric");
@@ -151,12 +153,13 @@ public class Similarity extends Application{
                 seriesE.getData().add(new XYChart.Data(i+1, Similarity.e[i]));
             }
             areaChart.getData().add(seriesE);
-
+            key = 1;
         });
-        Button imageButton2 = new Button("", new ImageView("cosine.jpg"));
+        Button imageButton2 = new Button("", new ImageView("img/cosine.jpg"));
         imageButton2.setPrefSize(200, 200);
         imageButton2.setOnMouseClicked(e->{
-            areaChart.getData().remove(0, 1);
+            if(areaChart.getData().size() == 1) areaChart.getData().remove(0, 1);
+            else areaChart.getData().remove(0, 2);
 //            areaChart.getData().removeAll();
             XYChart.Series seriesC = new XYChart.Series();
             seriesC.setName("CosineSimilarity");
@@ -164,12 +167,14 @@ public class Similarity extends Application{
                 seriesC.getData().add(new XYChart.Data(i+1, Similarity.c[i]));
             }
             areaChart.getData().add(seriesC);
+            key = 1;
 
         });
-        Button imageButton3 = new Button("", new ImageView("pearson.jpg"));
+        Button imageButton3 = new Button("", new ImageView("img/pearson.jpg"));
         imageButton3.setPrefSize(200, 200);
         imageButton3.setOnMouseClicked(e-> {
-                    areaChart.getData().remove(0, 1);
+            if(areaChart.getData().size() == 1) areaChart.getData().remove(0, 1);
+            else areaChart.getData().remove(0, 2);
 //            areaChart.getData().removeAll();
                     XYChart.Series seriesP = new XYChart.Series();
                     seriesP.setName("PearsonCorrelationScore");
@@ -177,6 +182,7 @@ public class Similarity extends Application{
                         seriesP.getData().add(new XYChart.Data(i + 1, Similarity.p[i]));
                     }
                     areaChart.getData().add(seriesP);
+                    key = 1;
         });
         buttomRightHBox.getChildren().addAll(imageButton1, imageButton2, imageButton3);
 
@@ -184,13 +190,9 @@ public class Similarity extends Application{
         rightVBox.getChildren().addAll(areaChart, buttomRightHBox);
         gridPane.add(rightVBox, 1, 0);
 
-        VBox leftVBox = new VBox();
-        leftVBox.setPadding(new Insets(10, 0, 0, 10));
-        leftVBox.getChildren().addAll(upperHBox, tableView);
-        gridPane.add(leftVBox, 0, 0);
 
         Scene scene  = new Scene(gridPane,1200,800);
-
+        scene.getStylesheets().add("CSS/Translate.css");
         stage.setScene(scene);
         stage.show();
 
